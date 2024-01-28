ecc369de0b2790cab3ab792f26d5865c24dd61fc
Diego Cuadros <cuadrosda21@gmail.com>
2024-01-28 14:06:50 -0800

    prepared for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,8 +10,6 @@ connectDB();
 // Init Middleware | gets the data in req.body in users.js
 app.use(express.json({ extended: false }));
 
-//starting main
-app.get('/', (req, res) => res.send('API Running'));
 
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -18,7 +17,20 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//Serve static assets in production
+//check for production
+if(process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'));
+
+  //serve index.html file
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 // setting up the port and running server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
